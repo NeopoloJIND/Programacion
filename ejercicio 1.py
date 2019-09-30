72ad5d8104789252f5938622977ab099b5c057d4
Neopolo <55462237+NeopoloJIND@users.noreply.github.com>
2019-09-29 23:57:40 -0500

    Add files via upload
    La función incorporada print() imprime una cadena en un fichero. Por defecto, este fichero es la salida estándar.

---
+++ ejercicio 1.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Sep 29 23:07:31 2019
+
+@author: poloXO
+"""
+print( " Neopolo Jarquin Jimenez "             ,       "        Ivan Colin Carpio         ")
+print (" Monte video ,Mz56 "                   ,       "                Brasil 12         ")
+print (" Col. Las canteras "                   ,       "                Mexico 86         " )              
+print (" Huixquilucan "                        ,       "                     Naucalpa      ")
+print (" 527770,Edo de Mex,MEXICO "            ,       "        53650 Edo Mex,Mexico        ")
