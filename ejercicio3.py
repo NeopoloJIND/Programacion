88c1724dd215dd07d699624704ddf1303b804837
Neopolo <55462237+NeopoloJIND@users.noreply.github.com>
2019-09-30 22:48:18 -0500

    Add files via upload

---
+++ ejercicio3.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Sep 30 22:36:27 2019
+
+@author: poloXO
+"""
+#Escribir un programa que le ´pregunte al usuario el largo y ancho de habitacion
+#una vez que los valores ahn sido leidos , su programa debe calcular y desplegar
+#el largo y ancho debe de ser introducido con punto flotante decimal 
+#incluida las unidades metros en su mensaje
+Largo = float ( input ("inserte el  largo en metros:"))
+Ancho = float ( input ("inserte  el ancho en metros:"))
+print ("El area de la habitacion es", Largo * Ancho, "m^2")
+ 
