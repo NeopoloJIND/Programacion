43026271a67b518ec756710193f17ebd580924a7
Neopolo <55462237+NeopoloJIND@users.noreply.github.com>
2019-11-15 13:14:20 -0600

    Add files via upload

---
+++ ejercicio46.py
@@ -0,0 +1,13 @@
+#Ejercicio 46
+#el año esta divido en cuatro temporadas: primavera, verano, otoño e invierno.
+#aunque las fechas exactas cambian un poco dependiendo del año, usemos las siguientes fechas.
+#temporada primer dia
+#_________________________
+#primavera	marzo 21
+#verano		junio 21
+#otoño		septiembre 22
+#invierno	diciembre 21
+#Esriba un programa en el que el usiario introduzca el mes y el dia. Eusuario introducira
+#el nombre del mes como una *string* seguido del dia como un entero *in*. su programa debe 
+#desplegar la temporda de acuerdo a la informacion aintroducida por el usuario.
+
