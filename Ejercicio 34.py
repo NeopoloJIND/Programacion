e4297071450fe1ef5a33a7c8c38a5fdff1e64568
Neopolo <55462237+NeopoloJIND@users.noreply.github.com>
2019-11-15 01:05:01 -0600

    Add files via upload

---
+++ Ejercicio 34.py
@@ -0,0 +1,10 @@
+#ejercicio34
+#Escriba un pro grama que lea un numero entero introducido por el usuario. Su programa debe desplegar
+#un mensaje indicando si su numero entero es par o impar.
+NumE= int(input("ingrese numero entero \n"))
+if NumE % 2==1:
+    print("Numero impar")
+else:
+    print("Numero par")
+    
+    
